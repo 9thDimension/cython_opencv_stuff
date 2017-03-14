488aeb41ae0897e04b9b6f2fb7d071ec77199461
Xavier <xavier.dcruz0@gmail.com>
2017-03-14 14:26:58 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/cython_opencv_stuff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cython_opencv_stuff.dir/main.cpp.o"
+  "cython_opencv_stuff.pdb"
+  "cython_opencv_stuff"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cython_opencv_stuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
